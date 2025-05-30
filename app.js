2c8410adbb54dbb344ca844d912f72dacfc95d38
Shambhavi Goyal <shambhavigoyal05@gmail.com>
2025-05-30 13:09:06 -0400

    finished

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ DELETE- api/tasks/:id - Delete Task*/
 
 const express= require('express');
 const app= express(); 
+const path = require('path')
+
 const tasks= require('./routes/tasks')
 const connectDB=require('./db/connect')
 require('dotenv').config()
@@ -14,15 +16,18 @@ const errorHandlerMiddleware=require('./middleware/error-handler')
 
 
 //middleware
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 //routes
-
 app.use('/api/v1/tasks', tasks) //For any request starting with /api/v1/tasks, pass control to the tasks router we imported from ./routes/tasks.
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
+// Serve frontend index.html for any other route (except /api routes)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 const port= process.env.PORT || 3000
 
 const start=async() =>{
